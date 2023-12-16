4cacc1f6b6beb42c52596ab8eff940bd05aaf982
Kate Vyshenskaia <115986016+kotyasmol@users.noreply.github.com>
2023-12-16 14:12:22 +0500

    Add files via upload

---
+++ task2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    //var11
+    internal class task2
+    {
+        static double ReadDouble()
+        {
+            double outputDoub = 0;
+            bool parsed = false;
+            do
+            {
+                try
+                {
+                    Console.Write("Enter number ");
+                    outputDoub = Convert.ToDouble(Console.ReadLine());
+                    parsed = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Error");
+                }
+            } while (!parsed);
+
+           return outputDoub;
+        }
+        static void Main()
+        {
+            Console.WriteLine("X1: ");
+            double x = ReadDouble();
+
+            Console.WriteLine("Y1: ");
+            double y = ReadDouble();
+
+            bool result = (Math.Pow(x, 2) + Math.Pow(y, 2) <= 1)
+                && (y<=0 && x<=0);
+            if (result) { Console.WriteLine(result);}
+            else { Console.WriteLine(result);}
+        }
+    }
+}
---
+++ task3.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class task3
+    {
+            static void Main(string[] args)
+            {
+                PrintMenu();
+            }
+
+            static void SolveFloat()
+            {
+            float result, a = 1000f, b = 0.0001f;
+
+            result = ((float)Math.Pow((a + b), 4) - (((float)Math.Pow(a, 4) + 4 * (float)Math.Pow(a, 3) * b))) / ((6 * (float)Math.Pow(a, 2) * (float)Math.Pow(b, 2)) + (float)Math.Pow(b, 4) + 4 * a * (float)Math.Pow(b, 3));
+            Console.WriteLine("результат вычислений: " + result);
+            }
+
+            static void SolveDouble()
+            {
+            double a = 1000, b = 0.0001, result;
+
+            result = (Math.Pow((a+b),4)-((Math.Pow(a,4)+4*Math.Pow(a,3)*b))) / ((6*Math.Pow(a,2)*Math.Pow(b,2)) + Math.Pow(b,4)+ 4*a*Math.Pow(b,3)); 
+            Console.WriteLine("результат вычислений: " + result);
+            }
+
+            static void PrintMenu()
+            {
+                string buf;
+                while (true)
+                {
+                    Console.WriteLine("Меню выбора задачи:");
+                    Console.WriteLine("1 - Решить с  чилами типа float");
+                    Console.WriteLine("2 - Решить с  чилами типа double");
+                    Console.WriteLine("3 - Выход");
+
+                    Console.Write("Введите номер задачи: ");
+                    buf = Console.ReadLine();
+
+                    if (buf == "3")
+                        break;
+
+                    switch (buf)
+                    {
+                        case "1":
+                            SolveFloat();
+                            break;
+
+                        case "2":
+                            SolveDouble();
+                            break;
+                      
+                        default:
+                            Console.WriteLine("Ошибка");
+                            break;
+                    }
+                }
+
+                Console.WriteLine("Завершение программы");
+            }
+        }
+    }
